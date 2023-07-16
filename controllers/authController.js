dd9d788c16bc8b70c34ebfe81894f7fcc6f2ce27
iamgufran <mgfzd786@gmail.com>
2023-07-16 11:05:08 +0530

    Add middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from '../models/userModel.js';
-import {hashPassword} from './../helpers/authHelper.js';
+import {comparePassword, hashPassword} from './../helpers/authHelper.js';
+import JWT from 'jsonwebtoken'
 
 export const registerController = async(req,res) => {
     try{
@@ -54,3 +55,64 @@ export const registerController = async(req,res) => {
         })
     }
 };
+
+//POST LOGIN
+export const loginController = async(req,res) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:'Invalid Email or Password'
+            })
+        }
+        //check if user exists
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'User not found, Please Register'
+            })
+                }        
+        const match = await comparePassword(password, user.password);
+        if(!match){
+            return res.status(404).send({
+                success:false,
+                message:'Invalid Password'
+            })
+        }
+        //token
+        const token = JWT.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '7 days'});
+        res.status(200).send({
+            success:true,
+            message:'User Logged In Successfully',
+            user:{
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+
+            },
+            token,
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in Login User',
+            error
+        })
+    }
+}
+
+//test controller
+export const testController = (req,res) => {
+    try{
+        res.send("Protected Routes");
+        }catch(error){
+        console.log(error)
+        res.send({error});
+
+        }
+};
+
