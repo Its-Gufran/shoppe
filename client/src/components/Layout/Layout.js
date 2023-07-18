ef43708592bf754889eec4df99b7c35b5552d317
iamgufran <mgfzd786@gmail.com>
2023-07-18 11:37:16 +0530

    Basic SEO

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-const Layout = (props) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
-        <Header />
-        <main style={{minHeight: "70vh"}}>{props.children}</main>
-        <Footer />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <div>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+        </div>
+
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
     </div>
-  )
+  );
 };
 
-export default Layout
+Layout.defaultProps = {
+  title: "Welcome to Shoppe",
+  description: "We sell the best products for cheap",
+  keywords: "electronics, buy electronics, cheap electronics",
+  author: "Shoppe",
+};
+
+export default Layout;
