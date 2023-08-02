76305c9512703641fc80da726793a6860827fd7c
iamgufran <mgfzd786@gmail.com>
2023-08-02 19:03:02 +0530

    Minor Changes

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
     try {
+        //a Promise is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
